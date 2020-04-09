6f33fd89e7a0cc47163fc6719eb14b04701d2e90
Heva <he.wu@linecorp.com>
2020-04-09 15:09:21 +0900

    Update output path

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# Current Unicode Version: 13.0.0
+# https://www.unicode.org/versions/Unicode13.0.0/ch18.pdf
+# https://www.unicode.org/Public/13.0.0/ucd/EastAsianWidth.txt
+# http://unicode.org/reports/tr11/
+
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_ROOT="$SCRIPT_DIR/.."
 INPUT_FILE="$SCRIPT_DIR/EastAsianWidth.txt"
-OUTPUT_DIR="$PROJECT_ROOT/EastAsianWidth/Sources/EastAsianWidth/Generated/UnicodeScalar+Extension.swift"
+OUTPUT_FILE="$PROJECT_ROOT/EastAsianWidth/Sources/EastAsianWidth/UnicodeScalar+Extension.swift"
 
 echo "Start Generating East Asian Width Unicode Swift File..."
-python3 "$SCRIPT_DIR/generate.py" $INPUT_FILE > $OUTPUT_DIR
+python3 "$SCRIPT_DIR/generate.py" $INPUT_FILE > $OUTPUT_FILE
 
 echo "Done..."
